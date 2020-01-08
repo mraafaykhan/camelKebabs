f4a5c4c4007d8919fe4a113708fc0c63ae3bd6a8
raafay khan <raafayrashid@icloud.com>
2020-01-09 00:45:29 +0500

    Adds camelToKebab function.
    -Code cleanup.
    -Adds code comments.
    -Removes crude test file.
    
    [PS-1]
    Init
    
    Signed-off-by: raafay khan <raafayrashid@icloud.com>

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-
+/*  
+* Function takes a javascript object and recursively applies the supplied formatter
+* function.
+* @Param object: javascript object that is going to be converted
+* @Param formatter: function that changes the notation of keys/attributes of the
+* object
+*/
 const changeCase = (object, formatter) => {
-  if (Array.isArray(object)) {
+  if (Array.isArray(object)) { // checks if the supplied object is an array
     return object = object.map(element => {
-      element = changeCase(element, formatter)
-      return element
+      // if the elements inside the array are objects ( including arrays ) then recursively apply the changeCase function
+      //  otherwise return the element ( primative types )
+      return typeof element === 'object' ? element = changeCase(element, formatter) : element
     })
   }
-  return Object.keys(object).reduce((obj, key) => {
-    
-    switch(true) {
-      case Array.isArray(object[key]):
-        obj[formatter(key)] = object[key].map(element => {
-
-          element = changeCase(element, formatter);
-          return element;
-        })
-      break;
-
-      case typeof object[key] === 'object':
-        obj[formatter(key)] = changeCase(object[key], formatter)
-        break;
-
-        default:
-          obj[formatter(key)] = object[key];
-    }
-    return obj
+  return Object.keys(object).reduce((obj, key) => { // iterate over the keys of the object
+    // if the key of the object refer to another nested object, then pass it recursively to the changeCase function
+    // otherwise if the key refers to a primitive value return the value
+    obj[formatter(key)] = typeof object[key] === 'object' ? changeCase(object[key], formatter) : object[key]
+    return obj;
   }, {})
 }
 
+/* 
+* Function converts an object from kebab case to JS friendly camel case
+* @Param obj: javascript object
+*/
 const kebabToCamel = (obj) => {
   return changeCase(obj, (key) => {
     return key.split('-').reduce((attribute, word, index)=> {
@@ -37,6 +34,25 @@ const kebabToCamel = (obj) => {
   })
 }
 
+/*
+* Function converts an object from camel case to kebab case notation
+* @Param obj: javascript object
+*/
+const camelToKebab = (obj) => {
+  return changeCase(obj, (key) => {
+    return key.split('').reduce((attribute, character, index) => {
+        if (index === 0) {
+          return attribute += character.toLowerCase()
+        }
+        if (character === character.toUpperCase()) {
+          return attribute += `-${character.toLowerCase()}`
+        }
+        return attribute += character   
+    }, '')
+  })
+}
+
 module.exports = {
+  camelToKebab: camelToKebab,
   kebabToCamel: kebabToCamel
 }
